a90b543582d6ca51ac6c3d60e72d6331a1d43a7c
Chelseaccc17 <chelseac0427@outlook.com>
2026-08-10 14:47:59 +0800

    Add: python-type-conversion-input - submission-0

---
+++ Python For Beginners/python-type-conversion-input/submission-0.py
@@ -0,0 +1,19 @@
+def read_integer() -> int:
+    age1 = int (input ())
+    return age1
+
+
+def read_float() -> float:
+    age2 = float (input ())
+    return age2
+
+
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
